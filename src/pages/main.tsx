a664d26374251079be2d4c8fc712c87f89bd4020
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-12 19:43:30 +0100

    feat: main post page setup

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { StoreState } from '../reducers'
 import {fetchPosts} from '../actions/actions'
 import { Box } from "@chakra-ui/core";
 import {postsReducerObject} from '../reducers/postsReducer'
+import Posts from '../components/Posts';
 
 interface MainComponentProps {
     fetchPosts(): any
@@ -11,12 +12,14 @@ interface MainComponentProps {
 }
 
 function Main({fetchPosts, posts}: MainComponentProps) {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [postsPerPage, setPostsPerPage] = useState(6)
     useEffect(() => {
         fetchPosts()
     }, [])
   return (
     <Box>
-
+        <Posts posts={posts} />
     </Box>
   )
 }
