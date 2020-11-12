34913dd066d9b2bc80e2f1189393595ea5a14af5
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-11-13 00:57:44 +0100

    test: unit test for main component

--- src/__tests__/Main.test.tsx
+++ src/__tests__/Main.test.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import Main from '../pages/main'
 import { render } from '../utils/render'
 
-describe('<Board />', () => {
-  test('Renders all the elements', async (done) => {
+describe('<Main />', () => {
+  test('Renders all the elements', async () => {
     const container = render(<Main />)
     // ???
     const { findByText, findByTestId } = container
     expect(await findByText('Copyright 2020')).toBeVisible()
-    // expect(await findByText('Board')).toBeVisible()
-    // expect(await findByTestId('trello_view')).toBeVisible()
-    // expect(await findByText('Activity')).toBeVisible()
-    // expect(await findByText('Archived Tasks')).toBeVisible()
-    done()
+    expect(await findByText('Epower Blog')).toBeVisible()
   })
 })
